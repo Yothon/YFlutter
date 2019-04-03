aa1b1634f3d753f118ed4b7f6b92272a26fa71ec
yothon <wanyouxin729@163.com>
2019-04-03 15:56:55 +0800

    branch1 commit

--- lib/DrawLinePage.dart
+++ lib/DrawLinePage.dart
@@ -1,62 +1,146 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter/rendering.dart' as ui;
+import 'package:flutter/services.dart' as ui;
+import 'package:flutter/widgets.dart' as ui;
+import 'package:flutter/material.dart' as ui;
+import 'package:flutter/painting.dart' as ui;
+import 'dart:ui' as flutterui;
 
-class DrawLinePage extends StatelessWidget{
-
+class DrawLinePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        title: Text("Path",style: TextStyle(color: Colors.white),),
+        title: Text(
+          "Path",
+          style: TextStyle(color: Colors.white70),
+        ),
         centerTitle: true,
         iconTheme: IconThemeData(color: Colors.white),
       ),
       body: DrawLineState(),
     );
   }
-
 }
 
-class DrawLineState extends StatefulWidget{
+class DrawLineState extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return DrawLineItem();
   }
-
 }
 
-class DrawLineItem extends State<DrawLineState>{
+class DrawLineItem extends State<DrawLineState> {
+  var image;
+  @override
+  void initState() {
+    super.initState();
+    ImageLoader.load("images/bg.jpg").then((image){
+      setState(() {
+        this.image =image;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: CustomPaint(painter: DrawLineView())
-    );
+    return Container(child: CustomPaint(painter: DrawLineView(image)));
   }
-
 }
 
+class DrawLineView extends CustomPainter {
+  var image;
 
-class DrawLineView extends CustomPainter{
+  DrawLineView(this.image);
+
   Paint _paint = Paint()
     ..isAntiAlias = true
     ..strokeWidth = 4.0
     ..style = PaintingStyle.stroke
-    ..color = Colors.blueAccent
-    ..strokeCap = StrokeCap.round;
+    ..strokeCap = StrokeCap.round
+    ..color = Colors.blueAccent;
+
+    Paint _paint2 = Paint()
+    ..isAntiAlias = true
+    ..strokeWidth = 4.0
+    ..style = PaintingStyle.fill
+    ..strokeCap = StrokeCap.round
+    ..color = Colors.black;
 
   @override
   void paint(Canvas canvas, Size size) {
     Path path = Path();
-    path.moveTo(40, 40);
+    path.moveTo(140, 40);
+    path.relativeMoveTo(0, 20);
     path.lineTo(100, 200);
+    path.relativeLineTo(30, 30);
+    Rect rect = Rect.fromCircle(
+        center: Offset(path.getBounds().left, path.getBounds().bottom),
+        radius: 30);
+    path.arcTo(rect, 0, 3.1415926 / 2, false);
     canvas.drawPath(path, _paint);
+    Path path3 =Path();
+    var width = 300;
+    var height = 400;
+    path3.moveTo(width / 2, height / 4);
+    path3.cubicTo((width * 6) / 7, height / 9, (width * 12) / 13,
+        (height * 2) / 5, width / 2, (height * 7) / 12);
+    canvas.drawPath(path3, _paint2);
+    Path path2 = Path();
+    path2.moveTo(width / 2, height / 4);
+    path2.cubicTo(width / 7, height / 9, width / 13, (height * 2) / 5,
+        width / 2, (height * 7) / 12);
+    canvas.drawPath(path2, _paint2);
+
+    Path path4 =Path();
+    path4.lineTo(100, 0);
+    path4.lineTo(0, 100);
+    path4.close();
+    canvas.drawPath(path4, _paint2);
+    Path path5 =Path();
+    path5.moveTo(200,0);
+    path5.lineTo(200, 100);
+    path5.lineTo(100, 0);
+    path5.close();
+    canvas.drawPath(path5, _paint2);
+    Path path6 =Path();
+    path6.moveTo(0, 400);
+    path6.lineTo(0, 600);
+    path6.lineTo(200, 600);
+    path6.close();
+    canvas.drawPath(path6, _paint2);
+    Path path7 =Path();
+    path7.arcTo(Rect.fromCircle(center:Offset(200, 300),radius: 50), 0, 3.1415926*2, false);
+    canvas.drawPath(path7, _paint);
+    // canvas.drawImage(image, Offset(100, 100), _paint);
+
   }
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) {
     return false;
   }
+}
 
+
+class ImageLoader{
+  static ui.AssetBundle getAssetBundle() => (ui.rootBundle != null)
+      ? ui.rootBundle
+      : new ui.NetworkAssetBundle(new Uri.directory(Uri.base.origin));
+  static Future<flutterui.Image> load(String url) async {
+    ui.ImageStream stream = new ui.AssetImage(url, bundle: getAssetBundle())
+        .resolve(ui.ImageConfiguration.empty);
+    Completer<flutterui.Image> completer = new Completer<flutterui.Image>();
+    void listener(ui.ImageInfo frame, bool synchronousCall) {
+      final flutterui.Image image = frame.image;
+      completer.complete(image);
+      stream.removeListener(listener);
+    }
+
+    stream.addListener(listener);
+    return completer.future;
+  }
 }
--- lib/ViewPage.dart
+++ lib/ViewPage.dart
@@ -3,21 +3,23 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_app/DrawLinePage.dart';
+import 'package:flutter_app/LabelPage.dart';
 
 class ViewPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("View", style: TextStyle(color: Colors.white),),
+        title: Text(
+          "View",
+          style: TextStyle(color: Colors.white),
+        ),
         centerTitle: true,
         iconTheme: IconThemeData(color: Colors.white),
       ),
       body: ViewState(),
     );
   }
-
 }
 
 class ViewState extends StatefulWidget {
@@ -25,11 +27,9 @@ class ViewState extends StatefulWidget {
   State<StatefulWidget> createState() {
     return ViewItem();
   }
-
 }
 
 class ViewItem extends State<ViewState> {
-
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -38,18 +38,29 @@ class ViewItem extends State<ViewState> {
           margin: EdgeInsets.all(40),
           child: CustomPaint(painter: MyView()),
         ),
-        RaisedButton(onPressed: () {
-          Navigator.of(context).push(
-              MaterialPageRoute(builder: (context) => DrawLinePage()));
-        }, child: Text("Path"),)
+        RaisedButton(
+          onPressed: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (context) => DrawLinePage()));
+          },
+          child: Text("Path"),
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 50),
+          child: RaisedButton(
+            child: Text("label"),
+            onPressed: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => LabelPage()));
+            },
+          ),
+        )
       ],
     );
   }
-
 }
 
 class MyView extends CustomPainter {
-
   Paint _paint = Paint()
     ..color = Colors.blueAccent
     ..strokeCap = StrokeCap.round
@@ -64,7 +75,7 @@ class MyView extends CustomPainter {
     ..isAntiAlias = true
     ..strokeWidth = 5.0;
 
-  double PI = 3.1415926;
+  double pi = 3.1415926;
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -73,21 +84,24 @@ class MyView extends CustomPainter {
     canvas.drawLine(Offset(30, -30), Offset(30, 30), _paint);
     canvas.drawLine(Offset(30, 30), Offset(-30, 30), _paint);
     canvas.drawLine(Offset(-30, 30), Offset(-30, -30), _paint);
-    canvas.drawPoints(PointMode.points, [
-      Offset(20, 20),
-      Offset(100.0, 100.0),
-      Offset(100.0, 200.0),
-      Offset(200.0, 200.0),
-      Offset(200.0, 100.0),
-      Offset(280.0, 20.0),
-      Offset(20, 20),
-    ], _paint);
+    canvas.drawPoints(
+        PointMode.points,
+        [
+          Offset(20, 20),
+          Offset(100.0, 100.0),
+          Offset(100.0, 200.0),
+          Offset(200.0, 200.0),
+          Offset(200.0, 100.0),
+          Offset(280.0, 20.0),
+          Offset(20, 20),
+        ],
+        _paint);
     canvas.drawOval(Rect.fromLTRB(40, 260, 200, 340), _paint);
     canvas.drawOval(Rect.fromPoints(Offset(140, 100), Offset(100, 80)), _paint);
     canvas.drawCircle(Offset(150, 100), 40, _paint);
     canvas.drawOval(Rect.fromLTWH(0, 360, 300, 100), _paint);
     Rect rect = Rect.fromPoints(Offset(40, 40), Offset(200, 200));
-    canvas.drawArc(rect, 0, PI / 6, false, _redPaint);
+    canvas.drawArc(rect, 0, pi / 6, false, _redPaint);
     Rect rect2 = Rect.fromCircle(center: Offset(100, 100), radius: 60);
     RRect rRect = RRect.fromRectAndRadius(rect2, Radius.circular(60));
     canvas.drawRRect(rRect, _redPaint);
@@ -97,5 +111,4 @@ class MyView extends CustomPainter {
   bool shouldRepaint(CustomPainter oldDelegate) {
     return false;
   }
-
 }
